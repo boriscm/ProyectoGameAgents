db4d130cb0b48e57105aaca8a1cbb936a94adf26
boriscm <b.castellanos@uniandes.edu.co>
2023-03-15 21:52:41 -0500

    modificaciones

--- Galaxian_game_agent.py
+++ Galaxian_game_agent.py
@@ -29,6 +29,18 @@ for episode in range(1,episodes+1):
     print('Episode: {} score:{}'.format(episode, score))
 env.close()
 
+def eval_genomes():
+    
+    score = 0
+
+    data = {}
+    env.render()
+    action = random.choice([0,1,2,3,4,5])
+    n_state = data[0]
+    reward = data[1] 
+    done_data = (data[2], data[3])
+    info = data[4]
+    score += reward
 
 def run(config_path):
     config = neat.config.Config(
